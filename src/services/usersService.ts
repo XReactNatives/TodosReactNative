fefa165dd9898732662a3d15c4ef0139f244909c
pengchengxiang25 <pengchengxiang25@gmail.com>
2024-12-27 01:25:08 +0800

    更新fetchUsersFromAPI

---
+++ src/services/usersService.ts
@@ -0,0 +1,18 @@
+// Users API 请求
+import { apiConfig } from "../config/apiConfig";
+import type { User } from "../types/users";
+
+const usersApiUrl = `${apiConfig.baseURL}/users`;
+
+export const fetchUsersFromAPI = async (): Promise<User[]> => {
+    const response = await fetch(usersApiUrl, {
+        method: "GET",
+    });
+
+    if (!response.ok) {
+        throw new Error("Network response was not ok");
+    }
+
+    const data: User[] = await response.json();
+    return data;
+}; 
